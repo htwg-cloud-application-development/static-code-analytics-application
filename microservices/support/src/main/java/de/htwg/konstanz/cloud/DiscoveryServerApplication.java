3bfe7a6773734181f81ec45395fa9e068d7ea1f3
Timi <ruprecht.t@gmx.de>
2016-04-19 23:23:18 +0200

    add basic spring boot app

---
+++ microservices/support/src/main/java/de/htwg/konstanz/cloud/DiscoveryServerApplication.java
@@ -0,0 +1,12 @@
+package de.htwg.konstanz.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryServerApplication.class, args);
+	}
+}
---
+++ microservices/support/src/test/java/de/htwg/konstanz/cloud/DiscoveryServerApplicationTests.java
@@ -0,0 +1,16 @@
+package de.htwg.konstanz.cloud;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = DiscoveryServerApplication.class)
+public class DiscoveryServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
