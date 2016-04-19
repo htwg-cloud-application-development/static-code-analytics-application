6deab06f024f5ff985b3d7732c36afc45b578237
Timi <ruprecht.t@gmx.de>
2016-04-19 23:31:32 +0200

    configure spring boot app as eureka server

--- microservices/support/src/main/java/de/htwg/konstanz/cloud/DiscoveryServerApplication.java
+++ microservices/support/src/main/java/de/htwg/konstanz/cloud/DiscoveryServerApplication.java
@@ -2,11 +2,13 @@ package de.htwg.konstanz.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class DiscoveryServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DiscoveryServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryServerApplication.class, args);
+    }
 }
