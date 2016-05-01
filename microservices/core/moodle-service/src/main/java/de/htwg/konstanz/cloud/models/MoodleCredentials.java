9dc6493eb9aee07636ad7dc69d2a0cc638457946
SteffenGorenflo <SteffenGorenflo@gmail.com>
2016-05-01 15:47:58 +0200

    add model for moodle credentials

---
+++ microservices/core/moodle-service/src/main/java/de/htwg/konstanz/cloud/models/MoodleCredentials.java
@@ -0,0 +1,12 @@
+package de.htwg.konstanz.cloud.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
+public class MoodleCredentials {
+
+    @JsonProperty("password")
+    private String passwort;
+    private String mail;
+}
