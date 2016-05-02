cf072568424d99d0a520e2ac3b8fa1acb8456a07
SteffenGorenflo <SteffenGorenflo@gmail.com>
2016-05-02 10:54:34 +0200

    improve validation

--- microservices/core/moodle-service/src/main/java/de/htwg/konstanz/cloud/models/MoodleCredentials.java
+++ microservices/core/moodle-service/src/main/java/de/htwg/konstanz/cloud/models/MoodleCredentials.java
@@ -1,8 +1,10 @@
 package de.htwg.konstanz.cloud.models;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Email;
 
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 @Data
 public class MoodleCredentials {
@@ -10,5 +12,6 @@ public class MoodleCredentials {
     @NotNull
     private String password;
     @NotNull
+    @Email
     private String mail;
 }
