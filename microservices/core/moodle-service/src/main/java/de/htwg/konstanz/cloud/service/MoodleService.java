16fb9db825cd66c4188b9d96ceb1acc6bb854029
SteffenGorenflo <SteffenGorenflo@gmail.com>
2016-05-02 10:47:46 +0200

    use validation at route

--- microservices/core/moodle-service/src/main/java/de/htwg/konstanz/cloud/service/MoodleService.java
+++ microservices/core/moodle-service/src/main/java/de/htwg/konstanz/cloud/service/MoodleService.java
@@ -1,10 +1,13 @@
 package de.htwg.konstanz.cloud.service;
 
+
 import de.htwg.konstanz.cloud.models.MoodleCredentials;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+
+import javax.validation.Valid;
 
 
 // to create a RESTful Controller (add Controller and ResponseBody)
@@ -19,7 +22,7 @@ public class MoodleService {
 
 
     @RequestMapping(value = "/update", method = RequestMethod.POST)
-    public ResponseEntity<?> getCourses(@RequestBody MoodleCredentials input) {
+    public ResponseEntity<Price> getCourses(@Valid @RequestBody MoodleCredentials input) {
 
 
         // make a request to moodle in order to obtain the token
