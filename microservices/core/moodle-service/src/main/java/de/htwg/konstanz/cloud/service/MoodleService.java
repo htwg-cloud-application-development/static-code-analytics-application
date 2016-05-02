33d902e88461bee3a582415ead3f20aca2698675
SteffenGorenflo <SteffenGorenflo@gmail.com>
2016-05-02 11:07:15 +0200

    introduce hystrix

--- microservices/core/moodle-service/src/main/java/de/htwg/konstanz/cloud/service/MoodleService.java
+++ microservices/core/moodle-service/src/main/java/de/htwg/konstanz/cloud/service/MoodleService.java
@@ -1,10 +1,15 @@
 package de.htwg.konstanz.cloud.service;
 
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import de.htwg.konstanz.cloud.models.MoodleCredentials;
+import de.htwg.konstanz.cloud.models.Price;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import javax.validation.Valid;
@@ -15,23 +20,31 @@ import javax.validation.Valid;
 public class MoodleService {
 
 
-	@RequestMapping("/info")
-	public String info() {
+    @RequestMapping("/info")
+    public String info() {
         return "Moode-Service";
-     }
+    }
 
 
     @RequestMapping(value = "/update", method = RequestMethod.POST)
     public ResponseEntity<Price> getCourses(@Valid @RequestBody MoodleCredentials input) {
 
 
-        // make a request to moodle in order to obtain the token
-        RestTemplate req = new RestTemplate();
-        //req.postForObject("moodle", )
+        getMoodleStuff();
 
+        Price price = new Price(23.23);
 
-        return new ResponseEntity<Object>("", HttpStatus.OK);
+        return new ResponseEntity<Price>(new Price(123.23), HttpStatus.OK);
 
+    }
+
+    @HystrixCommand(fallbackMethod = "getDefaultMoodleStuff")
+    private Object getMoodleStuff() {
+        return null;
+    }
+
+    private Object getDefaultMoodleStuff() {
+        return null;
     }
 
 }
