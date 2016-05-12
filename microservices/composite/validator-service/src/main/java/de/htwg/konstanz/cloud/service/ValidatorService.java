78de0f55c19508d43d9ab845737d3b178fb1355b
Timi <ruprecht.t@gmx.de>
2016-05-12 17:45:52 +0200

    add validate route

--- microservices/composite/validator-service/src/main/java/de/htwg/konstanz/cloud/service/ValidatorService.java
+++ microservices/composite/validator-service/src/main/java/de/htwg/konstanz/cloud/service/ValidatorService.java
@@ -1,6 +1,8 @@
 package de.htwg.konstanz.cloud.service;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,4 +13,9 @@ public class ValidatorService {
         return "validator-Service";
     }
 
+
+    @RequestMapping(value = "/validate/{groupId}", method = RequestMethod.POST)
+    public String validateGroup(@RequestBody String data) {
+        return null;
+    }
 }
