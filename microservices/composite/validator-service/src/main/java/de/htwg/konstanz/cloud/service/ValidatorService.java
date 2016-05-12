2e48c54df15337cada0c556a0b92adb56d544cb2
Timi <ruprecht.t@gmx.de>
2016-05-12 20:58:49 +0200

    get service instance with client side load balancing (ribbon)

--- microservices/composite/validator-service/src/main/java/de/htwg/konstanz/cloud/service/ValidatorService.java
+++ microservices/composite/validator-service/src/main/java/de/htwg/konstanz/cloud/service/ValidatorService.java
@@ -1,14 +1,31 @@
 package de.htwg.konstanz.cloud.service;
 
 import de.htwg.konstanz.cloud.model.ValidationData;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.PostConstruct;
+import java.net.URI;
 
 @RestController
 public class ValidatorService {
 
+    @Autowired
+    private LoadBalancerClient loadBalancer;
+
+    private RestTemplate restTemplate;
+
+    @PostConstruct
+    private void init() {
+        this.restTemplate = new RestTemplate();
+    }
+
     @RequestMapping("/info")
     public String info() {
-        return "validator-Service";
+        return "Validator-Service";
     }
 
 
@@ -25,6 +42,26 @@ public class ValidatorService {
 
     @RequestMapping(value = "/validate", method = RequestMethod.POST, consumes = "application/json")
     public String validateGroup(@RequestBody ValidationData data) {
-        return data.getRepositoryUrl();
+        // TODO POST github url
+        String response = getRequestObject("checkstyle-service", "/checkstyle", String.class);
+
+        // TODO search for checkstyle services - eureka
+        // TODO select random instance - ribbon
+        // TODO call service asynchronously - hystrix
+
+        return response;
+    }
+
+    protected <T> T getRequestObject(String serviceId, String requestURI, Class<T> type) {
+        String getUserURL = getRequestUrl(serviceId, requestURI);
+        return restTemplate.getForObject(getUserURL, type);
+
+    }
+
+    protected String getRequestUrl(String serviceId, String requestURI) {
+        // get service URL for message-service
+        ServiceInstance instance = loadBalancer.choose(serviceId);
+        // build request URL
+        return instance.getUri() + requestURI;
     }
 }
