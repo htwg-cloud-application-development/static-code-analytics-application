f27f91309c2f98da32bae1b0a0a39ff0e2d5fa06
Timi <ruprecht.t@gmx.de>
2016-05-14 00:33:24 +0200

    Implement request mapping for validate route and parse json object

--- microservices/core/checkstyle-service/src/main/java/de/htwg/konstanz/cloud/service/CheckstyleService.java
+++ microservices/core/checkstyle-service/src/main/java/de/htwg/konstanz/cloud/service/CheckstyleService.java
@@ -1,25 +1,35 @@
 package de.htwg.konstanz.cloud.service;
 
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.amazonaws.util.json.JSONException;
+import com.amazonaws.util.json.JSONObject;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 
 // to create a RESTful Controller (add Controller and ResponseBody)
 @RestController
 public class CheckstyleService {
 
-    @RequestMapping("/checkstyle")
-    public String hello() {
-    	String githubUrl = "";
-		CheckGitRep oCheckGitRep = new CheckGitRep();
-		oCheckGitRep.startIt();
-        return "Hello Moodle";
+    @RequestMapping(value = "/validate", method = RequestMethod.POST)
+    public ResponseEntity validate(@RequestBody String data) {
+        try {
+            JSONObject jsonObj = new JSONObject(data);
+            String repositoryUrl = jsonObj.getString("repositoryUrl");
+            CheckGitRep oCheckGitRep = new CheckGitRep();
+            String json = oCheckGitRep.startIt();
+            return ResponseEntity.ok(json);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+
+
     }
 
     @RequestMapping("/info")
     public String info() {
-        return "Helloooooooo :-)";
+        return "Checkstyle-Service)";
     }
-	
+
 }
