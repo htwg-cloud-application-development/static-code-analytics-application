3daee255a458d5169357cf0645035292ecb93c32
Timi <ruprecht.t@gmx.de>
2016-05-14 01:52:23 +0200

    add sample call to checkstyle service

--- microservices/composite/validator-service/src/main/java/de/htwg/konstanz/cloud/service/ValidatorService.java
+++ microservices/composite/validator-service/src/main/java/de/htwg/konstanz/cloud/service/ValidatorService.java
@@ -1,9 +1,12 @@
 package de.htwg.konstanz.cloud.service;
 
+import com.amazonaws.util.json.JSONException;
+import com.amazonaws.util.json.JSONObject;
 import de.htwg.konstanz.cloud.model.ValidationData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
@@ -41,25 +44,31 @@ public class ValidatorService {
     }
 
     @RequestMapping(value = "/validate", method = RequestMethod.POST, consumes = "application/json")
-    public String validateGroup(@RequestBody ValidationData data) {
-        // TODO POST github url
-        String response = getRequestObject("checkstyle-service", "/checkstyle", String.class);
+    public ResponseEntity<String> validateGroup(@RequestBody ValidationData data) {
+        JSONObject json = new JSONObject();
+        try {
+            json.put("repositoryUrl", "https://github.com/T1m1/de.htwg.se.monopoly");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        ResponseEntity<String> entity = restTemplate.postForEntity(getRequestUrl("checkstyle-service", "/validate"), json.toString(), String.class);
 
         // TODO search for checkstyle services - eureka
         // TODO select random instance - ribbon
         // TODO call service asynchronously - hystrix
 
-        return response;
+        return entity;
     }
 
     protected <T> T getRequestObject(String serviceId, String requestURI, Class<T> type) {
         String getUserURL = getRequestUrl(serviceId, requestURI);
         return restTemplate.getForObject(getUserURL, type);
-
     }
 
     protected String getRequestUrl(String serviceId, String requestURI) {
         // get service URL for message-service
+        // TODO kann schief gehen - kein loadbalancer vorhanden - return error
         ServiceInstance instance = loadBalancer.choose(serviceId);
         // build request URL
         return instance.getUri() + requestURI;
