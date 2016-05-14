b1c3b5e6cb9c32c5f42e1d9ed9dbfe4c49e5e649
Timi <ruprecht.t@gmx.de>
2016-05-14 12:46:41 +0200

    enable eureka client

--- microservices/core/checkstyle-service/src/main/java/de/htwg/konstanz/cloud/CheckstyleServiceApplication.java
+++ microservices/core/checkstyle-service/src/main/java/de/htwg/konstanz/cloud/CheckstyleServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CheckstyleServiceApplication {
 
     public static void main(String[] args) {
