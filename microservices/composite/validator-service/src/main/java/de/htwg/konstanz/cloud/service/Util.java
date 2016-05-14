100e230078e41c36c43204ce8fab47e7eb514d06
Timi <ruprecht.t@gmx.de>
2016-05-14 13:11:52 +0200

    add response methods to util class

--- microservices/composite/validator-service/src/main/java/de/htwg/konstanz/cloud/service/Util.java
+++ microservices/composite/validator-service/src/main/java/de/htwg/konstanz/cloud/service/Util.java
@@ -1,13 +1,17 @@
 package de.htwg.konstanz.cloud.service;
 
+import com.amazonaws.util.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.PostConstruct;
+import java.util.HashMap;
 
 @Component
 public class Util {
@@ -21,4 +25,16 @@ public class Util {
         this.restTemplate = new RestTemplate();
     }
 
+
+    protected <T> ResponseEntity<T> createResponse(T body, HttpStatus httpStatus) {
+        return new ResponseEntity<>(body, httpStatus);
+    }
+
+    protected ResponseEntity<String> createErrorResponse(String errorMessage, HttpStatus status) {
+        LOG.error(errorMessage);
+        HashMap<String, String> errorResponse = new HashMap<String, String>();
+        errorResponse.put("error", errorMessage);
+        JSONObject errorResponseObject = new JSONObject(errorResponse);
+        return createResponse(errorResponseObject.toString(), status);
+    }
 }
