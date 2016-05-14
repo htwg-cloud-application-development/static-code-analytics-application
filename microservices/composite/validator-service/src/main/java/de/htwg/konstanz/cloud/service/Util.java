61ceec4218a53c325fc901577d3d2123e7f2e7b9
Timi <ruprecht.t@gmx.de>
2016-05-14 13:22:08 +0200

    refactor Util

--- microservices/composite/validator-service/src/main/java/de/htwg/konstanz/cloud/service/Util.java
+++ microservices/composite/validator-service/src/main/java/de/htwg/konstanz/cloud/service/Util.java
@@ -3,36 +3,25 @@ package de.htwg.konstanz.cloud.service;
 import com.amazonaws.util.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
 
-import javax.annotation.PostConstruct;
 import java.util.HashMap;
 
 @Component
-public class Util {
+class Util {
 
     private static final Logger LOG = LoggerFactory.getLogger(Util.class);
 
-    private RestTemplate restTemplate;
 
-    @PostConstruct
-    private void init() {
-        this.restTemplate = new RestTemplate();
-    }
-
-
-    protected <T> ResponseEntity<T> createResponse(T body, HttpStatus httpStatus) {
+    <T> ResponseEntity<T> createResponse(T body, HttpStatus httpStatus) {
         return new ResponseEntity<>(body, httpStatus);
     }
 
-    protected ResponseEntity<String> createErrorResponse(String errorMessage, HttpStatus status) {
+    ResponseEntity<String> createErrorResponse(String errorMessage, HttpStatus status) {
         LOG.error(errorMessage);
-        HashMap<String, String> errorResponse = new HashMap<String, String>();
+        HashMap<String, String> errorResponse = new HashMap<>();
         errorResponse.put("error", errorMessage);
         JSONObject errorResponseObject = new JSONObject(errorResponse);
         return createResponse(errorResponseObject.toString(), status);
