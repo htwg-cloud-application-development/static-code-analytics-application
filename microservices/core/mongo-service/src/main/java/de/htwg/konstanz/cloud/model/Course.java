866a19f19f2a012f3d7b0cec6e8a4e982155ec63
Timi <ruprecht.t@gmx.de>
2016-05-24 21:39:43 +0200

    implement course bean

---
+++ microservices/core/mongo-service/src/main/java/de/htwg/konstanz/cloud/model/Course.java
@@ -0,0 +1,15 @@
+package de.htwg.konstanz.cloud.model;
+
+import com.sun.javafx.beans.IDProperty;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+import java.util.List;
+
+@Data
+public class Course {
+    @Id
+    String courseId;
+    String name;
+    List<Group> groups;
+}
