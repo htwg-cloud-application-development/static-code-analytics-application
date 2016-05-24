5bd6bcdce2a4df640c2601dda7aae1bc8ffba908
Timi <ruprecht.t@gmx.de>
2016-05-24 21:39:57 +0200

    implement group bean

---
+++ microservices/core/mongo-service/src/main/java/de/htwg/konstanz/cloud/model/Group.java
@@ -0,0 +1,14 @@
+package de.htwg.konstanz.cloud.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+import java.util.List;
+
+@Data
+public class Group {
+    @Id
+    String groupId;
+    String name;
+}
