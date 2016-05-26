da03d356eb8d59e5f314df0f6664b727c78394fe
Timi <ruprecht.t@gmx.de>
2016-05-26 21:27:04 +0200

    Add database service

---
+++ microservices/composite/governance-service/src/main/java/de/htwg/konstanz/cloud/service/DatabaseService.java
@@ -0,0 +1,32 @@
+package de.htwg.konstanz.cloud.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.PostConstruct;
+import java.util.concurrent.Future;
+
+@Service
+public class DatabaseService {
+
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private LoadBalancerClient loadBalancer;
+
+    @PostConstruct
+    private void init() {
+        this.restTemplate = new RestTemplate();
+    }
+}
+
