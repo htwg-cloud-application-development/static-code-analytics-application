18e77a425dda1c812095436fdfcaa0d842fa215d
Timi <ruprecht.t@gmx.de>
2016-05-27 00:32:02 +0200

    add zuul and governance service to start script

--- runAllOnWindows.bat
+++ runAllOnWindows.bat
@@ -8,7 +8,7 @@ start /D "C:/" mongod --dbpath %MONGO_DB_PATH%
 
 :: support
 start /D "microservices/support/discovery-server" mvn spring-boot:run -Dspring.profiles.active=local
-::start /D "microservices/support/edge-server" mvn spring-boot:run -Dspring.profiles.active=local
+start /D "microservices/support/edge-server" mvn spring-boot:run -Dspring.profiles.active=local
 
 :: core
 start /D "microservices/core/checkstyle-service" mvn spring-boot:run -Dspring.profiles.active=local
@@ -16,4 +16,4 @@ start /D "microservices/core/mongo-service" mvn spring-boot:run -Dspring.profile
 
 :: composite
 start /D "microservices/composite/validator-service" mvn spring-boot:run -Dspring.profiles.active=local
-
+start /D "microservices/composite/governance-service" mvn spring-boot:run -Dspring.profiles.active=local
