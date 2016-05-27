63d5fa80c02fbc2ede3de14de041e3784c07ceea
Steffen Gorenflo <SteffenGorenflo@gmail.com>
2016-05-27 16:17:57 +0200

    adjust moodle credentials

--- microservices/core/moodle-service/src/main/java/de/htwg/konstanz/cloud/models/MoodleCredentials.java
+++ microservices/core/moodle-service/src/main/java/de/htwg/konstanz/cloud/models/MoodleCredentials.java
@@ -1,10 +1,8 @@
 package de.htwg.konstanz.cloud.models;
 
 import lombok.Data;
-import org.hibernate.validator.constraints.Email;
 
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
 
 @Data
 public class MoodleCredentials {
@@ -12,6 +10,5 @@ public class MoodleCredentials {
     @NotNull
     private String password;
     @NotNull
-    @Email
-    private String mail;
+    private String username;
 }
