95178bb21017aafefdeb2065ec4594a91781985d
floritron <Flori1234@web.de>
2016-05-28 13:54:30 +0200

    completed some issues, changed checkstyle directory structure

--- microservices/core/checkstyle-service/src/main/java/de/htwg/konstanz/cloud/service/CheckGitRep.java
+++ microservices/core/checkstyle-service/src/main/java/de/htwg/konstanz/cloud/service/CheckGitRep.java
@@ -5,9 +5,11 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
+
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.io.FileUtils;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.api.errors.InvalidRemoteException;
@@ -19,9 +21,11 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
 
 /**
@@ -29,16 +33,19 @@ import javax.xml.parsers.DocumentBuilderFactory;
  */
 public class CheckGitRep {
     
-	List<Class> lFormattedClassList = new ArrayList<Class>();
+	private List<Class> lFormattedClassList = new ArrayList<Class>();
+	private File oRepoDir;
 
-    public String startIt(String gitRepository) {
+    public String startIt(String gitRepository) throws IOException, ParserConfigurationException, SAXException {
 		JSONObject oJsonResult = null;
 
 		if(checkLocalCheckstyle() == true)
 		{
-				List<List<String>> lRepoList = downloadRepoAndGetPath(gitRepository);
+			List<List<String>> lRepoList = downloadRepoAndGetPath(gitRepository);
 			oJsonResult = checkStyle(lRepoList, gitRepository);
+			FileUtils.deleteDirectory(oRepoDir);
 		}
+		
         return oJsonResult.toString();
     }
 
@@ -46,12 +53,12 @@ public class CheckGitRep {
         List<List<String>> list = new ArrayList<List<String>>();
 		String directoryName = gitRepo.substring(gitRepo.lastIndexOf("/"), gitRepo.length()-1).replace(".","_");
         String localDirectory = "repositories/"  + directoryName + "_" + System.currentTimeMillis() +"/";
-
+		Git git = null;
 
         // TODO ueberprüfen ob Repo vorhanden bzw. Giturl okay
 		// TODO Fehler bei bereits vorhandenem GIT Repo beheben
         try {
-            Git git = Git.cloneRepository()
+            git = Git.cloneRepository()
                     .setURI(gitRepo)
                     .setDirectory(new File(localDirectory)).call();
         } catch (InvalidRemoteException e) {
@@ -84,11 +91,13 @@ public class CheckGitRep {
                 list.add(pathsSub);
             }
         }
-
+		/* Closing Object that we can delete the whole directory later */
+		git.getRepository().close();
+		
         return list;
     }
 
-		public boolean checkLocalCheckstyle()
+	public boolean checkLocalCheckstyle() throws MalformedURLException, IOException, FileNotFoundException
 	{
 		boolean bSuccess = false;
 		final String sCheckstyleJar = "checkstyle-6.17-all.jar";
@@ -107,49 +116,17 @@ public class CheckGitRep {
 		else
 		{
 			System.out.println("Checkstyle .jar doesnt exists, Starting download");
-			
-				try
-				{
-					oURL = new URL(sDownloadCheckStyleJar);
-				} 
-				catch (MalformedURLException e)
-				{
-					System.out.println("Invalid URL");
-				}
-
-				try
-				{
-					oReadableByteChannel = Channels.newChannel(oURL.openStream());
-				} 
-				catch (IOException e)
-				{
-					System.out.println("Can't open URL");
-				}
-
-				try
-				{
-					oFileOutput = new FileOutputStream(sCheckstyleJar);
-				} 
-				catch (FileNotFoundException e)
-				{
-					System.out.println("Did not find an output file");
-				}
-				
-				try
-				{
-					oFileOutput.getChannel().transferFrom(oReadableByteChannel, 0, Long.MAX_VALUE);
-					bSuccess = true;
-				} 
-				catch (IOException e)
-				{
-					System.out.println("Error while transferring bytes into the output file");
-				}
+			oURL = new URL(sDownloadCheckStyleJar);
+			oReadableByteChannel = Channels.newChannel(oURL.openStream());
+			oFileOutput = new FileOutputStream(sCheckstyleJar);
+			oFileOutput.getChannel().transferFrom(oReadableByteChannel, 0, Long.MAX_VALUE);
+			bSuccess = true;
 		}
 		
 		return bSuccess;
 	}
 	
-    public JSONObject checkStyle(List<List<String>> lRepoList, String gitRepository)
+    public JSONObject checkStyle(List<List<String>> lRepoList, String gitRepository) throws ParserConfigurationException, SAXException, IOException
 	{
         final String sCheckStylePath = "checkstyle-6.17-all.jar";
 		final String sRuleSetPath = "/sun_checks.xml";
@@ -158,31 +135,35 @@ public class CheckGitRep {
 		/* Listeninhalt kuerzen, um JSON vorbereiten */
 		formatList(lRepoList);
 
-		for(int nClassPos = 0; nClassPos < lFormattedClassList.size(); nClassPos++) {
-				String sFullPath = lFormattedClassList.get(nClassPos).sFullPath;
-			
-				if (sFullPath.endsWith(".java")) {
-					sFullPath = sFullPath.substring(0, sFullPath.length() - 5);
-				}
-			
-				String sCheckStyleCommand = "java -jar " + sCheckStylePath + " -c " + sRuleSetPath + " " + sFullPath + ".java -f xml -o " + sFullPath + ".xml";
-
-				try {
-					Runtime runtime = Runtime.getRuntime();
-					Process proc = runtime.exec(sCheckStyleCommand);
-					
-					try { 
-						proc.waitFor(); 
-					} 
-					catch (InterruptedException e) { }
-				}
-				catch (IOException ex) { }
+		for(int nClassPos = 0; nClassPos < lFormattedClassList.size(); nClassPos++) 
+		{
+			String sFullPath = lFormattedClassList.get(nClassPos).getFullPath();
 		
-				/* Checkstyle Informationen eintragen */
-				storeCheckstyleInformation(sFullPath + ".xml", nClassPos);
+			if (sFullPath.endsWith(".java")) 
+			{
+				sFullPath = sFullPath.substring(0, sFullPath.length() - 5);
 			}
 		
-		if(lFormattedClassList != null)	{
+			String sCheckStyleCommand = "java -jar " + sCheckStylePath + " -c " + sRuleSetPath + " " + sFullPath + ".java -f xml -o " + sFullPath + ".xml";
+
+			try 
+			{
+				Runtime runtime = Runtime.getRuntime();
+				Process proc = runtime.exec(sCheckStyleCommand);
+				
+				try { 
+					proc.waitFor(); 
+				} 
+				catch (InterruptedException e) { }
+			}
+			catch (IOException ex) { }
+	
+			/* Checkstyle Informationen eintragen */
+			storeCheckstyleInformation(sFullPath + ".xml", nClassPos);
+		}
+		
+		if(lFormattedClassList != null)	
+		{
 			/* Schoene einheitliche JSON erstellen */
 			oJson = buildJSON(gitRepository);
 		}
@@ -190,64 +171,57 @@ public class CheckGitRep {
         return oJson;
     }
 
-    public void storeCheckstyleInformation(String sXmlPath, int nClassPos)
+    public void storeCheckstyleInformation(String sXmlPath, int nClassPos) throws ParserConfigurationException, SAXException, IOException
 	{
-		try 
-		{
-			File oFileXML = new File(sXmlPath);
-			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-			Document doc = dBuilder.parse(oFileXML);
-			doc.getDocumentElement().normalize();
-					
-			NodeList nList = doc.getElementsByTagName("error");
+		File oFileXML = new File(sXmlPath);
+		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+		Document doc = dBuilder.parse(oFileXML);
+		doc.getDocumentElement().normalize();
+				
+		NodeList nList = doc.getElementsByTagName("error");
 
-			for (int nNodePos = 0; nNodePos < nList.getLength(); nNodePos++) 
+		for (int nNodePos = 0; nNodePos < nList.getLength(); nNodePos++) 
+		{
+
+			Node nNode = nList.item(nNodePos);
+					 
+			if (nNode.getNodeType() == Node.ELEMENT_NODE) 
 			{
+				Element eElement = (Element) nNode;
+				
+				/* Default Values */
+				int nLine = 0;
+				int nColumn = 0;
+				String sSeverity = "";
+				String sMessage = "";
+				String sSource = "";
 
-				Node nNode = nList.item(nNodePos);
-						 
-				if (nNode.getNodeType() == Node.ELEMENT_NODE) 
+				if(isParsable(eElement.getAttribute("line")))
 				{
-					Element eElement = (Element) nNode;
-					
-					/* Default Values */
-					int nLine = 0;
-					int nColumn = 0;
-					String sSeverity = "";
-					String sMessage = "";
-					String sSource = "";
-
-					if(isParsable(eElement.getAttribute("line")))
-					{
-						nLine = Integer.parseInt(eElement.getAttribute("line"));
-					}
-					if(isParsable(eElement.getAttribute("column")))
-					{
-						nColumn = Integer.parseInt(eElement.getAttribute("column"));
-					}	
-					if(!eElement.getAttribute("severity").isEmpty())
-					{
-						sSeverity = eElement.getAttribute("severity");
-					}	
-					if(!eElement.getAttribute("message").isEmpty())
-					{
-						sMessage = eElement.getAttribute("message");
-					}
-					if(!eElement.getAttribute("source").isEmpty())
-					{
-						sSource = eElement.getAttribute("source");
-					}
-					
-					Error oError = new Error(nLine, nColumn, sSeverity, sMessage, sSource);
-					
-					lFormattedClassList.get(nClassPos).getErrorList().add(oError);
+					nLine = Integer.parseInt(eElement.getAttribute("line"));
 				}
+				if(isParsable(eElement.getAttribute("column")))
+				{
+					nColumn = Integer.parseInt(eElement.getAttribute("column"));
+				}	
+				if(!eElement.getAttribute("severity").isEmpty())
+				{
+					sSeverity = eElement.getAttribute("severity");
+				}	
+				if(!eElement.getAttribute("message").isEmpty())
+				{
+					sMessage = eElement.getAttribute("message");
+				}
+				if(!eElement.getAttribute("source").isEmpty())
+				{
+					sSource = eElement.getAttribute("source");
+				}
+				
+				Error oError = new Error(nLine, nColumn, sSeverity, sMessage, sSource);
+				
+				lFormattedClassList.get(nClassPos).getErrorList().add(oError);
 			}
-		} 
-		catch (Exception e) 
-		{
-			e.printStackTrace(); 
 		}
 	}
 
@@ -269,16 +243,20 @@ public class CheckGitRep {
 	
 	public void formatList(List<List<String>> lRepoList)
 	{
-		for(int nExcercisePos = 0; nExcercisePos < lRepoList.size(); nExcercisePos++)
+		for (int nExcercisePos = 0; nExcercisePos < lRepoList.size(); nExcercisePos++)
 		{
 			Class oClass = null;
-			
-			for(int x = 0; x < lRepoList.get(nExcercisePos).size(); x++)
+
+			for (int nClassPos = 0; nClassPos < lRepoList.get(nExcercisePos).size(); nClassPos++)
 			{
-				String[] need = lRepoList.get(nExcercisePos).get(x).split("\\\\");
-				String sFullPath = lRepoList.get(nExcercisePos).get(x);
+				String[] sFullPathSplit_a = lRepoList.get(nExcercisePos).get(nClassPos).split("\\\\");
+				String sFullPath = lRepoList.get(nExcercisePos).get(nClassPos);
+
+				String sTmpClassName = sFullPathSplit_a[sFullPathSplit_a.length - 1].toString();
+				String sTmpExerciseName = sFullPathSplit_a[1].toString();
 				
-				oClass = new Class(need[need.length-1].toString(), sFullPath);
+				oClass = new Class(sTmpClassName, sFullPath, sTmpExerciseName);
+				
 				lFormattedClassList.add(oClass);
 			}
 		}
@@ -288,7 +266,13 @@ public class CheckGitRep {
 	{
 		List<Error> lTmpErrorList = new ArrayList<Error>();
 		JSONObject oJsonRoot = new JSONObject();
+		JSONObject oJsonExercise = new JSONObject();
 		JSONArray lJsonClasses = new JSONArray();
+		JSONArray lJsonExercises = new JSONArray();
+		String sTmpExcerciseName = "";
+		boolean bExcerciseChange = false;
+		boolean bLastRun = false;
+		boolean bExcerciseNeverChanged = true;
 		
 		/* add general information to the JSON object */
 		oJsonRoot.put("repositoryUrl", sRepo);
@@ -296,32 +280,83 @@ public class CheckGitRep {
 		//oJsonRoot.put("name", sName);
 		
 		/* all Classes */
-		for(int nClassPos = 0; nClassPos < lFormattedClassList.size(); nClassPos++)
+		for (int nClassPos = 0; nClassPos < lFormattedClassList.size(); nClassPos++)
 		{
-			lTmpErrorList = lFormattedClassList.get(nClassPos).getErrorList();	
-			JSONArray lJsonErrors = new JSONArray();
-			JSONObject oJsonClass = new JSONObject();
-				
-			/* all Errors */
-			for(int nErrorPos = 0; nErrorPos < lTmpErrorList.size(); nErrorPos++)
+			if(bExcerciseChange == true)
 			{
-				JSONObject oJsonError = new JSONObject();
-				
-				oJsonError.put("line", Integer.toString(lTmpErrorList.get(nErrorPos).nErrorAtLine));
-				oJsonError.put("column", Integer.toString(lTmpErrorList.get(nErrorPos).nColumn));
-				oJsonError.put("severity", lTmpErrorList.get(nErrorPos).sSeverity);
-				oJsonError.put("message", lTmpErrorList.get(nErrorPos).sMessage);
-				oJsonError.put("source", lTmpErrorList.get(nErrorPos).sSource);
-				
-				lJsonErrors.put(oJsonError);
+				nClassPos--;
+				bExcerciseChange = false;
 			}
-			
-			oJsonClass.put("filepath", lFormattedClassList.get(nClassPos).sFullPath);
-			oJsonClass.put("errors", lJsonErrors);
-			lJsonClasses.put(oJsonClass);
+
+			String sExcerciseName = lFormattedClassList.get(nClassPos).getsExcerciseName();
+
+			/* first run the TmpName is empty */
+			if(sExcerciseName.equals(sTmpExcerciseName) || sTmpExcerciseName.equals(""))
+			{
+				lTmpErrorList = lFormattedClassList.get(nClassPos).getErrorList();
+				JSONArray lJsonErrors = new JSONArray();
+				JSONObject oJsonClass = new JSONObject();
+
+				/* all Errors */
+				for (int nErrorPos = 0; nErrorPos < lTmpErrorList.size(); nErrorPos++)
+				{
+					JSONObject oJsonError = new JSONObject();
+
+					oJsonError.put("line", Integer
+							.toString(lTmpErrorList.get(nErrorPos).getErrorAtLine()));
+					oJsonError.put("column",
+							Integer.toString(lTmpErrorList.get(nErrorPos).getColumn()));
+					oJsonError.put("severity",
+							lTmpErrorList.get(nErrorPos).getSeverity());
+					oJsonError.put("message",
+							lTmpErrorList.get(nErrorPos).getMessage());
+					oJsonError.put("source", lTmpErrorList.get(nErrorPos).getSource());
+
+					lJsonErrors.put(oJsonError);
+				}
+
+				sTmpExcerciseName = sExcerciseName;
+				
+				oJsonClass.put("filepath", lFormattedClassList.get(nClassPos).getFullPath());
+				oJsonClass.put("errors", lJsonErrors);
+				lJsonClasses.put(oJsonClass);
+				
+				/* last run if different exercises were found */
+				if(bLastRun == true)
+				{
+					oJsonExercise.put(sTmpExcerciseName, lJsonClasses);
+					lJsonExercises.put(oJsonExercise);
+				}
+				
+				/* last run if there was just one exercise */
+				if((nClassPos + 1) == lFormattedClassList.size() && bExcerciseNeverChanged == true)
+				{
+					oJsonExercise.put(sTmpExcerciseName, lJsonClasses);
+					lJsonExercises.put(oJsonExercise);
+				}
+			}
+			/* swap for a different exercise */
+			else
+			{
+				oJsonExercise.put(sTmpExcerciseName, lJsonClasses);
+				lJsonExercises.put(oJsonExercise);
+				oJsonExercise = new JSONObject();
+				lJsonClasses = new JSONArray();
+				sTmpExcerciseName = lFormattedClassList.get(nClassPos).getsExcerciseName();
+				bExcerciseChange = true;
+				bExcerciseNeverChanged = false;
+				
+				/* decrement the position to get the last class from the list */
+				if((nClassPos + 1) == lFormattedClassList.size())
+				{
+					nClassPos--;
+					bExcerciseChange = false;
+					bLastRun = true;
+				}
+			}
 		}
-		
-		oJsonRoot.put("files", lJsonClasses);
+
+		oJsonRoot.put("files", lJsonExercises);
 		
 		return oJsonRoot;
 	}
--- microservices/core/checkstyle-service/src/main/java/de/htwg/konstanz/cloud/service/CheckstyleService.java
+++ microservices/core/checkstyle-service/src/main/java/de/htwg/konstanz/cloud/service/CheckstyleService.java
@@ -1,5 +1,15 @@
 package de.htwg.konstanz.cloud.service;
 
+import javax.xml.parsers.ParserConfigurationException;
+import java.net.MalformedURLException;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import org.xml.sax.SAXException;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.api.errors.InvalidRemoteException;
+import org.eclipse.jgit.api.errors.TransportException;
+
+
 import com.amazonaws.util.json.JSONException;
 import com.amazonaws.util.json.JSONObject;
 import org.springframework.http.HttpStatus;
@@ -13,7 +23,8 @@ public class CheckstyleService {
 
     @RequestMapping(value = "/validate", method = RequestMethod.POST)
     public ResponseEntity validate(@RequestBody String data) {
-        try {
+        try 
+		{
             JSONObject jsonObj = new JSONObject(data);
             String repositoryUrl = jsonObj.getString("repositoryUrl");
             // TODO übergeben der repository Url an Methode oder im Konstruktor zur Validierung - bsp url https://github.com/T1m1/de.htwg.se.monopoly
@@ -22,12 +33,37 @@ public class CheckstyleService {
             // TODO Wenn Methode schief läuft "InternalServerError" zurückliefern
             String json = oCheckGitRep.startIt(repositoryUrl);
             return ResponseEntity.ok(json);
-        } catch (Exception e) {
-            e.printStackTrace();
+        }
+		catch (ParserConfigurationException e) 
+		{
+            e.getMessage();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+		catch (MalformedURLException e) 
+		{
+            e.getMessage();
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+		catch (FileNotFoundException e) 
+		{
+            e.getMessage();
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+		catch (SAXException e) 
+		{
+            e.getMessage();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+		catch (IOException e) 
+		{
+            e.getMessage();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+		catch (Exception e) 
+		{
+            e.getMessage();
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
         }
-
-
     }
 
     @RequestMapping("/info")
