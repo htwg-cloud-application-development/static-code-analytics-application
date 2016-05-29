f4e7213a9ddf297ce1ce617e5fb998e28f2a96a1
Timi <ruprecht.t@gmx.de>
2016-05-29 12:39:56 +0200

    add swagger

--- microservices/core/checkstyle-service/src/main/java/de/htwg/konstanz/cloud/CheckstyleServiceApplication.java
+++ microservices/core/checkstyle-service/src/main/java/de/htwg/konstanz/cloud/CheckstyleServiceApplication.java
@@ -4,12 +4,43 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.PathSelectors.regex;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableSwagger2
 public class CheckstyleServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CheckstyleServiceApplication.class, args);
+    }
+
+    // swagger dashboard "/swagger-ui.html"
+    @Bean
+    public Docket newsApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("validate")
+                .apiInfo(apiInfo())
+                .select()
+                .paths(regex("/.*"))
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Checkstyle Service")
+                .description("Handle service calls for validation services")
+                .termsOfServiceUrl("http://www-03.ibm.com/software/sla/sladb.nsf/sla/bm?Open")
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://github.com/IBM-Bluemix/news-aggregator/blob/master/LICENSE")
+                .version("2.0")
+                .build();
     }
 }
