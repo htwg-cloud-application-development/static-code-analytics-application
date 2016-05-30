d0ebdf099e7e498fcbf008bf5aab1d1e744a64c6
Max <maxhildner@fastmail.com>
2016-05-30 17:36:07 +0200

    included lombok

--- microservices/core/mongo-service/src/main/java/de/htwg/konstanz/cloud/model/Error.java
+++ microservices/core/mongo-service/src/main/java/de/htwg/konstanz/cloud/model/Error.java
@@ -1,47 +1,13 @@
 package de.htwg.konstanz.cloud.model;
 
+import lombok.Data;
+
+@Data
 public class Error {
-	
+
 	private String line;
 	private String column;
 	private String severity;
 	private String message;
 	private String source;
-	
-	public String getSeverity() {
-		return severity;
-	}
-	public void setSeverity(String severity) {
-		this.severity = severity;
-	}
-	public String getLine() {
-		return line;
-	}
-	public void setLine(String line) {
-		this.line = line;
-	}
-	public String getColumn() {
-		return column;
-	}
-	public void setColumn(String column) {
-		this.column = column;
-	}
-	public String getSource() {
-		return source;
-	}
-	public void setSource(String source) {
-		this.source = source;
-	}
-	public String getMessage() {
-		return message;
-	}
-	public void setMessage(String message) {
-		this.message = message;
-	}
-	
-	
-	
-	
-	
-	
 }
