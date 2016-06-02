606ce4ec3c61f57446378877bc1e5c9478effafd
Steffen Gorenflo <SteffenGorenflo@gmail.com>
2016-06-02 14:56:06 +0200

    add pojo for assignment

---
+++ microservices/core/moodle-service/src/main/java/de/htwg/konstanz/cloud/models/MoodleAssignment.java
@@ -0,0 +1,19 @@
+package de.htwg.konstanz.cloud.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+/**
+ * Created by steffen on 02/06/16.
+ */
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class MoodleAssignment {
+
+    Integer id;
+    String name;
+    String modicon;
+    String modname;
+    String modplural;
+    Integer indent;
+}
