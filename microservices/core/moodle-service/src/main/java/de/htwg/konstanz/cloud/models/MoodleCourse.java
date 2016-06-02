54067201cae7d65d9fed05abba3d589482bb574c
Steffen Gorenflo <SteffenGorenflo@gmail.com>
2016-06-02 12:57:24 +0200

    add pojo for moodle course

--- microservices/core/moodle-service/src/main/java/de/htwg/konstanz/cloud/models/MoodleCourse.java
+++ microservices/core/moodle-service/src/main/java/de/htwg/konstanz/cloud/models/MoodleCourse.java
@@ -1,7 +1,19 @@
 package de.htwg.konstanz.cloud.models;
 
+import lombok.Data;
+
 /**
  * Created by steffen on 27/05/16.
  */
+@Data
 public class MoodleCourse {
+
+    Integer id;
+    String shortname;
+    String fullname;
+    Integer enrolledusercount;
+    String idnumber;
+    Integer visible;
+
 }
+
