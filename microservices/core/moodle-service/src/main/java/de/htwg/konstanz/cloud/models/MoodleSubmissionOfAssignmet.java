b2d87a4f5c0393c8b6de726f1011ad3a079cdbfb
Steffen Gorenflo <SteffenGorenflo@gmail.com>
2016-06-02 19:33:18 +0200

    add pojo for submission of assignment

---
+++ microservices/core/moodle-service/src/main/java/de/htwg/konstanz/cloud/models/MoodleSubmissionOfAssignmet.java
@@ -0,0 +1,23 @@
+package de.htwg.konstanz.cloud.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * Created by steffen on 02/06/16.
+ */
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class MoodleSubmissionOfAssignmet {
+
+    Integer id;
+    Integer userid;
+    Integer attemptnumber;
+    Integer timecreated;
+    Integer timemodified;
+    String status;
+    Integer groupid;
+    String repository;
+}
