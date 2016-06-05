0b6a4117f432c43024b066eef921b3186da0112d
Timi <ruprecht.t@gmx.de>
2016-06-05 10:20:52 +0200

    enable config server

--- microservices/support/config-server/src/main/java/de/htwg/cloud/ConfigServerApplication.java
+++ microservices/support/config-server/src/main/java/de/htwg/cloud/ConfigServerApplication.java
@@ -3,10 +3,11 @@ package de.htwg.cloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableConfigServer
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
