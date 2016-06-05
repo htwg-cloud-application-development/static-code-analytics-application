c95451e91e1eee830019a2ca63ed339f483ecb49
Timi <ruprecht.t@gmx.de>
2016-06-05 13:31:41 +0200

    remove eureka

--- microservices/support/config-server/src/main/java/de/htwg/cloud/ConfigServerApplication.java
+++ microservices/support/config-server/src/main/java/de/htwg/cloud/ConfigServerApplication.java
@@ -6,7 +6,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 @EnableConfigServer
 public class ConfigServerApplication {
 
