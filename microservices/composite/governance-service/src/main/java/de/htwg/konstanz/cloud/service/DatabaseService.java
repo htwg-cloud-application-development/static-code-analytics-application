c4ffa693d80417a66742495d141ed658578d9e74
Steffen Gorenflo <SteffenGorenflo@gmail.com>
2016-06-07 16:51:59 +0200

    implement skeleton to save groups to database

--- microservices/composite/governance-service/src/main/java/de/htwg/konstanz/cloud/service/DatabaseService.java
+++ microservices/composite/governance-service/src/main/java/de/htwg/konstanz/cloud/service/DatabaseService.java
@@ -1,20 +1,11 @@
 package de.htwg.konstanz.cloud.service;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.scheduling.annotation.Async;
-import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.PostConstruct;
-import java.util.concurrent.Future;
 
 @Service
 public class DatabaseService {
@@ -59,6 +50,10 @@ public class DatabaseService {
 
     private String postDataBaseFor(String route, String data) throws InstantiationException {
         return util.postToService(route, data, "mongo");
+    }
+
+    public String saveGroups(Integer courseid, String groups) {
+        return null;
     }
 }
 
