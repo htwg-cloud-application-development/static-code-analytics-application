de702903fe675c199aa7b08fb84c62b3e861da0e
Max <maxhildner@fastmail.com>
2016-06-07 09:17:11 +0200

    courses route adjusted

--- microservices/core/mongo-service/src/main/java/de/htwg/konstanz/cloud/model/User.java
+++ microservices/core/mongo-service/src/main/java/de/htwg/konstanz/cloud/model/User.java
@@ -2,6 +2,7 @@ package de.htwg.konstanz.cloud.model;
 
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
@@ -21,6 +22,6 @@ public class User {
     String userid;
     String siteurl;
     String userpictureurl;
+    @DBRef
     List<Course> courses;
-
 }
