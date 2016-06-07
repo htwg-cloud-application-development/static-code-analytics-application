54c03ce479dfe59657a965fadfa0ef9821534b1f
Timi <ruprecht.t@gmx.de>
2016-06-07 18:43:35 +0200

    blablablabla blub

--- microservices/composite/validator-service/src/main/java/de/htwg/konstanz/cloud/service/ValidatorService.java
+++ microservices/composite/validator-service/src/main/java/de/htwg/konstanz/cloud/service/ValidatorService.java
@@ -1,12 +1,11 @@
 package de.htwg.konstanz.cloud.service;
 
-import com.amazonaws.auth.InstanceProfileCredentialsProvider;
+import com.amazonaws.auth.EnvironmentVariableCredentialsProvider;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.ec2.AmazonEC2;
 import com.amazonaws.services.ec2.AmazonEC2Client;
 import com.amazonaws.services.ec2.model.Instance;
-import com.amazonaws.services.ec2.model.Region;
 import com.amazonaws.util.json.JSONArray;
-import com.amazonaws.util.json.JSONException;
 import com.amazonaws.util.json.JSONObject;
 import de.htwg.konstanz.cloud.model.ValidationData;
 import io.swagger.annotations.ApiOperation;
@@ -25,10 +24,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-import java.util.concurrent.FutureTask;
 
 @RestController
 @EnableAsync
@@ -57,12 +53,16 @@ public class ValidatorService {
     @RequestMapping(value = "/info", method = RequestMethod.GET, produces = "application/json")
     public String info() {
         // - read number of available services (Checkstyle, etc)
-        InstanceProfileCredentialsProvider tmp = new InstanceProfileCredentialsProvider();
+        //InstanceProfileCredentialsProvider tmp = new InstanceProfileCredentialsProvider();
+        EnvironmentVariableCredentialsProvider tmp = new EnvironmentVariableCredentialsProvider();
+        //ProfileCredentialsProvider tmp3 = new ProfileCredentialsProvider();
         LOG.info(tmp.getCredentials().toString());
         StringBuilder sb = new StringBuilder();
         sb.append(tmp.getCredentials().toString());
 
         AmazonEC2 ec2 = new AmazonEC2Client(tmp);
+        ec2.setRegion(com.amazonaws.regions.Region.getRegion(Regions.EU_CENTRAL_1));
+
 
         List<Instance> alleMeineEntchen = util.getAllActiveInstances(ec2);
         for (Instance instance : alleMeineEntchen) {
