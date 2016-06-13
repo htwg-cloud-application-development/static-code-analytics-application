efb05e173b2d1c495cdfb019c475339a0bbd0cf1
Timi <ruprecht.t@gmx.de>
2016-06-13 22:34:18 +0200

    BUGFIX

--- microservices/composite/validator-service/src/main/java/de/htwg/konstanz/cloud/service/ValidatorService.java
+++ microservices/composite/validator-service/src/main/java/de/htwg/konstanz/cloud/service/ValidatorService.java
@@ -122,8 +122,8 @@ public class ValidatorService {
             ServiceInstance pmdInstance = loadBalancer.choose("pmd");
 
             long startTime = System.currentTimeMillis();
-            Future<String> checkstyleRepo = validateRepositoryService.validateRepository(jsonObject.getString("repository"), checkstyleInstance.getUri());
-            Future<String> pmdRepo = validateRepositoryService.validateRepository(jsonObject.getString("repository"), pmdInstance.getUri());
+            Future<String> checkstyleRepo = validateRepositoryService.validateRepository(jsonObject.toString(), checkstyleInstance.getUri());
+            Future<String> pmdRepo = validateRepositoryService.validateRepository(jsonObject.toString(), pmdInstance.getUri());
 
             boolean run = true;
             // Wait until they are done
