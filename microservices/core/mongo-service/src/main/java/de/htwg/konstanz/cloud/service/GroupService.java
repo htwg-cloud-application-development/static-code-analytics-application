6a638280599c9eb592216989c633a859b4b62474
Steffen Gorenflo <SteffenGorenflo@gmail.com>
2016-06-13 08:32:44 +0200

    add logging statements

--- microservices/core/mongo-service/src/main/java/de/htwg/konstanz/cloud/service/GroupService.java
+++ microservices/core/mongo-service/src/main/java/de/htwg/konstanz/cloud/service/GroupService.java
@@ -19,14 +19,16 @@ public class GroupService {
     private CourseRepository courseRepo;
 
     @RequestMapping(path = "/{courseId}", method = RequestMethod.POST, consumes = "application/json")
-    public void create(@RequestBody List<Group> groups, @PathVariable String courseId)throws NoSuchFieldException {
+    public void create(@RequestBody List<Group> groups, @PathVariable String courseId) throws NoSuchFieldException {
 
+        System.out.println(groups);
         if (groups.isEmpty()) { // don't save empty groups
+            System.out.println("no groups, retun");
             return;
         }
 
         Course course = courseRepo.findOne(courseId);
-        if(null == course){
+        if (null == course) {
             throw new NoSuchFieldException("Course not found");
         }
 
