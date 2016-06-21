18a6a11c0fa429d052b7d60e595dd153387786ed
Timi <ruprecht.t@gmx.de>
2016-06-21 23:43:32 +0200

    fix pmd and checkstyle warnings

--- microservices/composite/validator-service/src/main/java/de/htwg/konstanz/cloud/service/CustomScheduler.java
+++ microservices/composite/validator-service/src/main/java/de/htwg/konstanz/cloud/service/CustomScheduler.java
@@ -79,9 +79,9 @@ public class CustomScheduler {//NOPMD
         return startScheduling(groups, fullExecutionTime, pipeline, repoUserInformationMap);
     }
 
-    private ArrayList<JSONObject> startScheduling(JSONArray groups, int fullExecutionTime, Map<Integer,
+    private ArrayList<JSONObject> startScheduling(JSONArray groups, int fullExecutionTime, Map<Integer,//NOPMD
             List<JSONObject>> pipeline, Map<String, String> repoUserInformationMap) throws NoSuchFieldException,
-            JSONException, InterruptedException, ExecutionException, InstantiationException {//NOPMD
+            JSONException, InterruptedException, ExecutionException, InstantiationException {
 
         AmazonEC2 ec2 = new AmazonEC2Client(new EnvironmentVariableCredentialsProvider());
         ec2.setRegion(com.amazonaws.regions.Region.getRegion(Regions.EU_CENTRAL_1));
