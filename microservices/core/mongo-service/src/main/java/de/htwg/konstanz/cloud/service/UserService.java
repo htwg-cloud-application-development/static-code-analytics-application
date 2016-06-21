a0b4c566b49ece5908f94ea16445f6224984a555
Max <maxhildner@fastmail.com>
2016-06-21 15:10:56 +0200

    ResponseEntity UserService

--- microservices/core/mongo-service/src/main/java/de/htwg/konstanz/cloud/service/UserService.java
+++ microservices/core/mongo-service/src/main/java/de/htwg/konstanz/cloud/service/UserService.java
@@ -3,6 +3,8 @@ package de.htwg.konstanz.cloud.service;
 import de.htwg.konstanz.cloud.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,13 +18,14 @@ public class UserService {
     UserRepository userRepo;
 
     @RequestMapping(value = "/add", method = RequestMethod.POST)
-    public void addUser(@RequestBody final User user) {
+    public ResponseEntity addUser(@RequestBody final User user) {
 
         userRepo.save(user);
+        return new ResponseEntity(HttpStatus.OK);
     }
 
     @RequestMapping(value = "{userId}", method = RequestMethod.GET)
-    public User getUser(@PathVariable final String userId) {
-        return userRepo.findOne(userId);
+    public ResponseEntity<User> getUser(@PathVariable final String userId) {
+        return new ResponseEntity<User>(userRepo.findOne(userId), HttpStatus.OK);
     }
 }
