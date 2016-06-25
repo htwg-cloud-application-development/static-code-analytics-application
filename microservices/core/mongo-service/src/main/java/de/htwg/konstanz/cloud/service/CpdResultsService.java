4af925508f2c8bcab590a75554d19e77d2e7f91f
Max <maxhildner@fastmail.com>
2016-06-25 15:46:30 +0200

    /getLastCpdResults

--- microservices/core/mongo-service/src/main/java/de/htwg/konstanz/cloud/service/CpdResultsService.java
+++ microservices/core/mongo-service/src/main/java/de/htwg/konstanz/cloud/service/CpdResultsService.java
@@ -1,16 +1,20 @@
 package de.htwg.konstanz.cloud.service;
 
+import com.netflix.ribbon.proxy.annotation.Http;
 import de.htwg.konstanz.cloud.model.CpdResults;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.nio.file.Path;
 import java.util.Date;
+import java.util.List;
 
 /**
  * Created by max on 24/06/16.
@@ -20,6 +24,9 @@ import java.util.Date;
 public class CpdResultsService {
 
     @Autowired
+    MongoOperations mongo;
+
+    @Autowired
     CpdResultsRepository cpdResultsRepository;
 
     @RequestMapping(value = "/addcpdresult", method = RequestMethod.POST ,consumes = "application/json")
@@ -27,9 +34,19 @@ public class CpdResultsService {
         ResponseEntity responseEntity;
 
         cpdResults.setTimestamp(String.valueOf(String.valueOf(new Date().getTime())));
-
         cpdResultsRepository.save(cpdResults);
-
         return new ResponseEntity(HttpStatus.OK);
+    }
+
+
+    @RequestMapping(value = "/getLastCpdResult/{courseId}", method = RequestMethod.GET)
+    public ResponseEntity<CpdResults> getLastCpdResult(@PathVariable("courseId") final String courseId){
+
+        Query query = new Query();
+        query.addCriteria(Criteria.where("courseId").is(courseId));
+        Sort sort = new Sort(Sort.Direction.DESC, "timestamp");
+        List<CpdResults> cpdResult = mongo.find(query.with(sort).limit(1), CpdResults.class);
+        return new ResponseEntity<>(cpdResult.get(0), HttpStatus.OK);
+
     }
 }
