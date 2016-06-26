b647421d85ccd6219c67d30c77b555ca2893b48a
Max <maxhildner@fastmail.com>
2016-06-26 11:24:14 +0200

    pmd and checkstyle

--- microservices/core/mongo-service/src/main/java/de/htwg/konstanz/cloud/service/GroupService.java
+++ microservices/core/mongo-service/src/main/java/de/htwg/konstanz/cloud/service/GroupService.java
@@ -3,18 +3,15 @@ package de.htwg.konstanz.cloud.service;
 import de.htwg.konstanz.cloud.model.Course;
 import de.htwg.konstanz.cloud.model.Group;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.geo.Circle;
 import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
-import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
 @RestController
 @RequestMapping("/groups")
@@ -31,16 +28,17 @@ public class GroupService {
 
     /**
      * Consumes a list of groups and either updates or creates entries
-     * @param groups new or updated groups
+     *
+     * @param groups   new or updated groups
      * @param courseId course which holds the gorups
-     * @return
+     * @return HttpStatus
      */
     @RequestMapping(path = "/{courseId}", method = RequestMethod.POST, consumes = "application/json")
-    public ResponseEntity create(@RequestBody List<Group> groups, @PathVariable final String courseId) {
+    public ResponseEntity create(@RequestBody final List<Group> groups, @PathVariable final String courseId) {
         ResponseEntity responseEntity;
 
         /** find course **/
-        Course course = courseRepo.findOne(courseId);
+        final Course course = courseRepo.findOne(courseId);
 
         /** don't save empty groups or if no course is found **/
         if (groups.isEmpty() || null == course) {
@@ -51,18 +49,38 @@ public class GroupService {
         else {
 
             /** iterate over group entries **/
-            for (Group group: groups){
+            for (final Group group : groups) {
 
                 /** querry for savedGroup that is already persisted and is the same as new group**/
                 /** then we only need to update **/
-                Query query = new Query();
+                final Query query = new Query();
                 query.addCriteria(Criteria.where("id").is(group.getId()));
-                Group savedGroup = mongo.findOne(query, Group.class);
+                final Group savedGroup = mongo.findOne(query, Group.class);
 
+
+                /** no match found, group is new entry **/
+                if (savedGroup == null) {
+                    /** persist new group **/
+                    groupRepository.save(group);
+
+                    /** if group is first entry fpr course, first a new array has to be created **/
+                    /** array with group has to be set to course **/
+                    if (course.getGroups() == null) {
+                        final List<Group> initGroupList = new ArrayList<>();
+                        initGroupList.add(group);
+                        course.setGroups(initGroupList);
+
+                        /** add new group to course **/
+                    } else {
+                        course.getGroups().add(group);
+                    }
+                    /** save updates **/
+                    courseRepo.save(course);
+                }
                 /** if match on db is found, just update group **/
                 /** don't update pmd and checkstyle **/
                 /** pmd and checkstyle are updated over different route **/
-                if (savedGroup != null){
+                else {
                     savedGroup.setAttemptnumber(group.getAttemptnumber());
                     savedGroup.setRepository(group.getRepository());
                     savedGroup.setStatus(group.getStatus());
@@ -70,26 +88,6 @@ public class GroupService {
                     savedGroup.setTimemodified(group.getTimemodified());
                     /** save updates **/
                     groupRepository.save(savedGroup);
-
-                /** no match found, group is new entry **/
-                } else {
-
-                    /** persist new group **/
-                    groupRepository.save(group);
-
-                    /** if group is first entry fpr course, first a new array has to be created **/
-                    /** array with group has to be set to course **/
-                    if (course.getGroups() == null){
-                        List<Group> initGroupList = new ArrayList<>();
-                        initGroupList.add(group);
-                        course.setGroups(initGroupList);
-
-                    /** add new group to course **/
-                    } else {
-                        course.getGroups().add(group);
-                    }
-                    /** save updates **/
-                    courseRepo.save(course);
                 }
             }
             responseEntity = new ResponseEntity(HttpStatus.OK);
@@ -99,6 +97,7 @@ public class GroupService {
 
     /**
      * Returns group entry belongig to this id
+     *
      * @param userId id of group to return
      * @return group
      */
