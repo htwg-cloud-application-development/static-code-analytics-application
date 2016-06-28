0bccd02d471bc02829603e6618ca639892c60f39
Timi <ruprecht.t@gmx.de>
2016-06-28 18:40:06 +0200

    add javadoc

--- microservices/composite/validator-service/src/main/java/de/htwg/konstanz/cloud/service/DatabaseService.java
+++ microservices/composite/validator-service/src/main/java/de/htwg/konstanz/cloud/service/DatabaseService.java
@@ -27,36 +27,70 @@ public class DatabaseService {
     @Autowired
     private LoadBalancerClient loadBalancer;
 
+    /**
+     * Constructor called before creation of service.
+     */
     @PostConstruct
     private void init() {//NOPMD
         this.restTemplate = new RestTemplate();
         restTemplate.getMessageConverters().add(0, new StringHttpMessageConverter(Charset.forName("UTF-8")));
     }
 
+    /**
+     * Save checkstyle and pmd Result to Database.
+     *
+     * @param result validation result converted form json object to string.
+     * @return result of database action.
+     */
     @Async
     public Future<String> saveResult(String result) throws InstantiationException {
         String route = "/addEntry";
         return addResultToDatabase(result, route);
     }
 
+    /**
+     * Save checkstyle result into database.
+     *
+     * @param result validation result converted form json object to string.
+     * @return result of database action.
+     */
     @Async
     public Future<String> saveCheckstleResult(String result) throws InstantiationException {
         String route = "/addCheckstyleEntry";
         return addResultToDatabase(result, route);
     }
 
+    /**
+     * Save pmd result into database.
+     *
+     * @param result validation result converted form json object to string.
+     * @return result of database action.
+     */
     @Async
     public Future<String> savePmdResult(String result) throws InstantiationException {
         String route = "/addPmdEntry";
         return addResultToDatabase(result, route);
     }
 
+    /**
+     * Save cpd  result into database.
+     *
+     * @param result validation result converted form json object to string.
+     * @return result of database action.
+     */
     @Async
     public Future<String> saveCpdResult(String result) throws InstantiationException {
         String route = "/cpdresults";
         return addResultToDatabase(result, route);
     }
 
+    /**
+     * Update 'exectiontime' of group. Neddet for scheduler calculation.
+     *
+     * @param duration duration of validation.
+     * @param id       database id of group
+     * @return result of database action.
+     */
     @Async
     public Future<String> updateExecutionTimeOfGroup(long duration, String id) throws JSONException, InstantiationException {
         String data = "{\"executiontime\": " + duration + "}";
@@ -64,6 +98,13 @@ public class DatabaseService {
         return addResultToDatabase(data, route);
     }
 
+    /**
+     * Method to save documents into mongodb. Get instance with round robin scheduler and execute POST.
+     *
+     * @param result    JSON object to save as String
+     * @param saveRoute route to call for adding json object to database.
+     * @return result of database action.
+     */
     private Future<String> addResultToDatabase(String result, String saveRoute) throws InstantiationException {
         // get database service instance
         ServiceInstance instance = loadBalancer.choose("mongo");
@@ -81,26 +122,62 @@ public class DatabaseService {
         throw new InstantiationException("service is not available");
     }
 
+    /**
+     * Get group by id.
+     *
+     * @param groupId group id-
+     * @return Json object as String. Needs to be convert.
+     */
     public String getGroup(String groupId) throws InstantiationException {
         return callDatabaseRoute("/groups/" + groupId);
     }
 
+    /**
+     * Get course by id.
+     *
+     * @param courseId course id-
+     * @return Json object as String. Needs to be convert.
+     */
     public String getCourse(String courseId) throws InstantiationException {
         return callDatabaseRoute("/courses/" + courseId);
     }
 
+    /**
+     * Get last checkstlye result of user with userid.
+     *
+     * @param userId id of user
+     * @return Json object as String. Needs to be convert.
+     */
     public String getLastCheckstyleResult(String userId) throws InstantiationException {
         return callDatabaseRoute("/courses/" + userId + "/findLastCheckstyleResult");
     }
 
+    /**
+     * Get last pmd result of user with userid.
+     *
+     * @param userId id of user
+     * @return Json object as String. Needs to be convert.
+     */
     public String getLastPmdResult(String userId) throws InstantiationException {
         return callDatabaseRoute("/courses/" + userId + "/findLastPmdResult");
     }
 
+    /**
+     * Get last cpd result of user with userid.
+     *
+     * @param userId id of user
+     * @return Json object as String. Needs to be convert.
+     */
     public String getLastCpdResult(String userId) throws InstantiationException {
         return callDatabaseRoute("/cpdresults/courses/" + userId + "/findLastCpdResult");
     }
 
+    /**
+     * Method to call Database with GET.
+     *
+     * @param serviceRoute Route to be called.
+     * @return Json Object as String.
+     */
     private String callDatabaseRoute(String serviceRoute) throws InstantiationException {
         ServiceInstance instance = loadBalancer.choose("mongo");
         if (null != instance) {
